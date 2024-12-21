0d2656111ea77aa4592435ae21a05ee63db1d020
Trieu Vu Ha <trieuvuha123@gmail.com>
2024-12-21 15:12:01 +0700

    #25 CRUD - Create

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,22 +1,34 @@
-import db from "../models"
+import db from "../models";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
-    try {
-        let data = await db.User.findAll();
-        return res.render("homepage.ejs", {
-            data: JSON.stringify(data)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-    
-}
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render("test/about.ejs")
-}
+  return res.render("test/about.ejs");
+};
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud");
+};
 
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
