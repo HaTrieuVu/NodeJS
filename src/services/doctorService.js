97c2d6b7acc037396ef5877f4862a497e5ff3497
Trieu Vu Ha <trieuvuha123@gmail.com>
2025-01-08 20:43:25 +0700

    #80 tạo quan hệ cho bảng

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 require("dotenv").config();
 
-import _ from "lodash";
+import _, { includes } from "lodash";
 import moment from "moment";
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
@@ -139,6 +139,17 @@ let getDetailDoctorByIdService = async (inputId) => {
                 include: [
                     { model: db.Markdown, attributes: ["description", "contentHTML", "contentMarkdown"] },
                     { model: db.Allcode, as: "positionData", attributes: ["valueEn", "valueVi"] },
+                    {
+                        model: db.Doctor_Infor,
+                        attributes: {
+                            exclude: ["id", "doctorId"],
+                        },
+                        include: [
+                            { model: db.Allcode, as: "priceTypeData", attributes: ["valueEn", "valueVi"] },
+                            { model: db.Allcode, as: "provinceTypeData", attributes: ["valueEn", "valueVi"] },
+                            { model: db.Allcode, as: "paymentTypeData", attributes: ["valueEn", "valueVi"] },
+                        ],
+                    },
                 ],
                 raw: false,
                 nest: true,
